45e9369bb0a4c200e2619fa44cb6883ca8128637
Sufyan Ali <sufyan.dev@gmail.com>
2026-01-27 13:36:05 +0500

    Update

--- github_pushing.cpp
+++ github_pushing.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main() {
 
     cout << "My name is sufyan ali " << endl;
-
+    cout << "Also my name is ali " << endl;
+    cout << "Push to the Github " <<  endl;
+ 
     return 0;
 }
