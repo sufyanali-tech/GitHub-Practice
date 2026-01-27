49410049d99ce72c83f5862b1ae6d60aab1faff8
Sufyan Ali <sufyan.dev@gmail.com>
2026-01-27 12:14:06 +0500

    6th commit

--- github_practice.cpp
+++ github_practice.cpp
@@ -10,7 +10,7 @@ int main() {
     cout << "Github 4th commit " << endl;
     cout << "Github 5th commit " << endl;
     cout << "Github 6th commit " << endl;
-    cout << "Github 7th commit " << endl;
+    
     
     return 0;
 }
